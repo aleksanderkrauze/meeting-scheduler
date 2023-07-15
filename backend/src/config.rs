c7e7fd30ca9942889f35cadef275f5818297e521
Aleksander Krauze <alkoszczak@gmail.com>
2023-07-15 16:05:08 +0200

    Connect database to axum app and fetch basic data

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -3,10 +3,13 @@ use std::sync::Arc;
 
 use anyhow::{Context, Result};
 
+#[derive(Debug, Clone)]
 pub struct Config {
-    postgres_user: String,
-    postgres_password: String,
-    postgres_db: String,
+    pub postgres_user: String,
+    pub postgres_password: String,
+    pub postgres_url: String,
+    pub postgres_port: u16,
+    pub postgres_db: String,
     pub server_port: u16,
 }
 
@@ -15,6 +18,11 @@ impl Config {
         let postgres_user = var("POSTGRES_USER").context("missing env variable POSTGRES_USER")?;
         let postgres_password =
             var("POSTGRES_PASSWORD").context("missing env variable POSTGRES_PASSWORD")?;
+        let postgres_url = var("POSTGRES_URL").context("missing env variable POSTGRES_URL")?;
+        let postgres_port = var("POSTGRES_PORT")
+            .context("missing env variable POSTGRES_PORT")?
+            .parse()
+            .context("failed to parse POSTGRES_PORT as u16")?;
         let postgres_db = var("POSTGRES_DB").context("missing env variable POSTGRES_DB")?;
         let server_port = var("SERVER_PORT")
             .context("missing env variable SERVER_PORT")?
@@ -24,10 +32,23 @@ impl Config {
         let config = Config {
             postgres_user,
             postgres_password,
+            postgres_url,
+            postgres_port,
             postgres_db,
             server_port,
         };
 
         Ok(Arc::new(config))
+    }
+
+    pub fn postgres_uri(&self) -> String {
+        format!(
+            "postgresql://{}:{}@{}:{}/{}",
+            self.postgres_user,
+            self.postgres_password,
+            self.postgres_url,
+            self.postgres_port,
+            self.postgres_db
+        )
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,26 @@
 mod config;
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    sync::Arc,
+    time::Duration,
+};
 
-use anyhow::Result;
-use axum::{routing::get, Router, Server};
+use anyhow::{Context, Result};
+use axum::{extract::State, http::StatusCode, response::Html, routing::get, Router, Server};
 use dotenvy::dotenv;
+use sqlx::{
+    postgres::{PgPool, PgPoolOptions},
+    Row,
+};
 
 use config::Config;
+
+#[derive(Debug, Clone)]
+struct AppState {
+    config: Arc<Config>,
+    database_pool: PgPool,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,7 +28,21 @@ async fn main() -> Result<()> {
 
     let config = Config::from_env()?;
 
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+    let database_pool = PgPoolOptions::new()
+        .max_connections(64)
+        .acquire_timeout(Duration::from_secs(2))
+        .connect(&config.postgres_uri())
+        .await
+        .context("failed to create db connection pool")?;
+
+    let app_state = AppState {
+        config: Arc::clone(&config),
+        database_pool,
+    };
+
+    let app = Router::new()
+        .route("/", get(homepage))
+        .with_state(app_state);
 
     Server::bind(&SocketAddr::new(
         IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
@@ -24,4 +52,29 @@ async fn main() -> Result<()> {
     .await?;
 
     Ok(())
+}
+
+#[axum_macros::debug_handler]
+async fn homepage(State(app_state): State<AppState>) -> Result<Html<String>, (StatusCode, String)> {
+    let results = sqlx::query("SELECT * FROM meetings")
+        .fetch_all(&app_state.database_pool)
+        .await
+        .context("fetching from database failed")
+        .map_err(internal_error)?
+        .into_iter()
+        .map::<String, _>(|row| row.get("name"));
+
+    let mut body = String::from("<h1>Meetings:</h1><ul>");
+    for meeting in results {
+        body.push_str("<li>");
+        body.push_str(&meeting);
+        body.push_str("</li>");
+    }
+    body.push_str("</ul>");
+
+    Ok(Html(body))
+}
+
+fn internal_error(err: anyhow::Error) -> (StatusCode, String) {
+    (StatusCode::INTERNAL_SERVER_ERROR, format!("{:#}", err))
 }
