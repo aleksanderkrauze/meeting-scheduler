3e1e86bb2a91d8524ec64348337f2dbde1ced0a8
Aleksander Krauze <alkoszczak@gmail.com>
2023-07-15 12:02:15 +0200

    Create basic hello world axum app

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,27 @@
 mod config;
 
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use anyhow::Result;
+use axum::{routing::get, Router, Server};
 use dotenvy::dotenv;
 
 use config::Config;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let _ = dotenv();
 
     let config = Config::from_env()?;
+
+    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+
+    Server::bind(&SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+        config.server_port,
+    ))
+    .serve(app.into_make_service())
+    .await?;
 
     Ok(())
 }
